8fbad1b9ccb3b2db680a6b21336adb09b7c7a4d5
zhuhe02 <zhuhe02@baidu.com>
2019-05-01 00:44:55 +0800

    update test script wc.py, added more comment

--- test/wc.py
+++ test/wc.py
@@ -34,12 +34,28 @@ if __name__ == '__main__':
         job.run()
         exit()
 
+
+    # call LocalRunner like this:
     # job.run(runner='local', input=os.path.join(cur_dir, 'data.txt'))
 
+
+    # reset hadoop python archive like this:
+    from mrjob.runner.hadoop import set_hadoop_python
+    set_hadoop_python(
+        '/user/ubs/pv/common/python272.tar.gz#python2.7.2',
+        'python2.7.2/python2.7/bin/python')
+
+
+    # call HadoopRunner like this
     job.run(
+        # you can reset hadoop client with `hadoop` argument
+        hadoop='/home/work/hadoop-client-yq/hadoop/bin/hadoop',
+
+        # other arguments is the same like `hadoop streaming`
         input='afs://tianqi.afs.baidu.com:9902/user/ubs/pv/common/feed_os_version.txt',
         output='afs://tianqi.afs.baidu.com:9902/user/ubs/pv/zhuhe02/tmp/test_mrjob/',
-        merge_output=1,
+
+        # `jobconf` argument is the same like `hadoop streaming -jobconf` or `hadoop streaming -D`
         jobconf={
             'mapred.job.name': 'zhuhe02_word_count_by_mrjob',
             'mapred.reduce.tasks': 1,
